12bc02349f0d0f4deabf3a9c67be3e919d6e6863
Phil Hauser <phil.hsr@gmail.com>
2014-10-17 19:18:46 +0100

    added dev site to gh pages

---
+++ dev/js/main.js
@@ -0,0 +1,75 @@
+/**
+ * @file Sets up RequireJS config and bootstraps the website
+ */
+
+var commonJS = typeof requirejs == 'undefined';
+var config = {
+	
+	'paths': {
+		'base':				'vendor/Base',
+		'jquery':			'vendor/jquery',
+		'text':				'vendor/require-text',
+		'elementquery':		'vendor/elementQuery',
+		'checkout':			'vendor/checkout',
+		'html5shiv':		'ie/html5shiv',
+	},
+	
+	'shim': {
+		'jquery': {
+			'exports': '$'
+		},
+		'elementquery': {
+			'deps': ['jquery'],
+			'exports': 'elementQuery'
+		},
+		'html5shiv':{
+			'exports' : 'h5s'
+		},
+		'checkout':{
+			'exports': 'checkout'
+		}
+	},
+	
+	'name': 'main',
+	'wrap': true
+	
+};
+
+
+// Loading via require or node?
+if (!commonJS) {
+	// Require, set up
+	requirejs.config(config);
+
+	requirejs([
+		'vendor/console',
+		'vendor/domReady',
+		'jquery',
+		'elementquery',
+		'html5shiv',
+		'componentList'
+	],
+		
+	function (consolePolyfill, domReady, $, eq, h5s, componentList) {
+		consolePolyfill.run();
+		console.log('[main.js] Website init');
+
+		// init element query
+		window.elementQuery.init();
+
+		// refresh element query on window resize
+		// NOTE: we use a jQuery plugin for the resize event
+		// because it it far less resource intensive.
+		$.windowResize(window.elementQuery.refresh);
+
+		domReady(function () {
+			// Get the list of components and 
+			// run their tasks
+			componentList.runComponentTasks();
+		});
+
+	});
+} else {
+	// Node, export config
+	module.exports = config;
+}
